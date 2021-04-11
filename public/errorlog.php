fe5d4fc573c77cc7c525d6b606d3cf5d5616b0cb
Magdyel Lopez <magdyel_136@hotmail.com>
2021-04-11 12:42:07 -0400

    Subida a Git

---
+++ public/errorlog.php
@@ -0,0 +1,10 @@
+<?php
+
+/* PHP error reporting level, if different from default */
+
+	//error_reporting( E_ALL & ~E_NOTICE & ~E_WARNING ); // show errors, debug mode
+	//ini_set("display_errors", 1);
+	//include("graves.php");
+
+	error_reporting(0); // dont show errors , production server
+?>
